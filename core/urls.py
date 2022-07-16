52d616f8b2be270194abef14490232577243ee1a
xiidot1303 <xii1303@inbox.ru>
2022-07-16 13:08:04 +0500

    add static url in urls.py

--- core/urls.py
+++ core/urls.py
@@ -12,7 +12,12 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+
 """
+
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.contrib import admin
 from django.urls import path
 from app.views import *
@@ -25,4 +30,4 @@ urlpatterns = [
     path('about', about, name='about'),
     path('service', service, name='service'),
     path('contact', contact, name='contact')
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
